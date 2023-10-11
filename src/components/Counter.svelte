7c70dda05d44c22f275c6af759c3e70ed275ef2b
k1nho <kinhong99@gmail.com>
2023-10-11 16:46:31 -0400

    reactive declarations

---
+++ src/components/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+	let count = 0;
+
+	function increment() {
+		count++;
+	}
+
+	// reactive declarations
+	$: doubled = count * 2;
+	$: if (count >= 10) {
+		alert('we have a high count!');
+		count = 0;
+	}
+</script>
+
+<div>
+	Counter : {count}
+	Double of Counter: {doubled}
+	<button on:click={increment}>Increment</button>
+</div>
