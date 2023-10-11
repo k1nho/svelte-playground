e78c2a86327527c896d08e1d9477530e959d3fb4
k1nho <kinhong99@gmail.com>
2023-10-11 15:51:11 -0400

    svelte app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let name = 'svelte app';
+	let src = './favicon.png';
+</script>
+
+<div>
+	<img {src} alt="this is an icon" />
+	<h1>hello {name.toUpperCase()}</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus ipsum quod neque
+		sit odio doloribus iusto dignissimos at repellat nostrum accusamus perspiciatis et aliquam.
+		Officiis harum alias odio ex?
+	</p>
+</div>
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
