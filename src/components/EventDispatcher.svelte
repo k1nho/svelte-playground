6a568c0a62ae86b3f754f503092a5142381f8596
k1nho <kinhong99@gmail.com>
2023-10-11 22:37:35 -0400

    event dispatchers

---
+++ src/components/EventDispatcher.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	function sayHello() {
+		dispatch('message', {
+			text: 'Hello from Event Dispatcher'
+		});
+	}
+</script>
+
+<div>
+	<button on:click={() => sayHello}>dispatch message event</button>
+</div>
