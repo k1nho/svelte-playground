5596a196e78ecd844f88c3d560b7c977e5c02de3
k1nho <kinhong99@gmail.com>
2023-10-12 00:22:01 -0400

    stores

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,20 +1,25 @@
-<script>
-	let count = 0;
+<script lang="ts">
+	import { count } from '../stores/counterstore';
 
 	function increment() {
-		count++;
+		count.update((x) => x + 1);
+	}
+
+	function decrement() {
+		count.update((x) => x - 1);
 	}
 
 	// reactive declarations
-	$: doubled = count * 2;
-	$: if (count >= 10) {
-		alert('we have a high count!');
-		count = 0;
+	$: doubled = $count * 2;
+	$: if ($count >= 10) {
+		alert('we have a high count! setting counter to 0');
+		count.set(0);
 	}
 </script>
 
 <div>
-	Counter : {count}
+	Counter : {$count}
 	Double of Counter: {doubled}
 	<button on:click={increment}>Increment</button>
+	<button on:click={decrement}>Decrement</button>
 </div>
---
+++ src/components/Subscriber.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { count } from '../stores/counterstore';
+</script>
+
+<div>
+	Subscriber component, tracking count {$count}
+</div>
